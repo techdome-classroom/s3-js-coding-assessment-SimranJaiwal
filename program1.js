59c7398a9efc120b8b0586988f2c33caef7f4dcc
SimranJaiwal <161817125+SimranJaiwal@users.noreply.github.com>
2024-10-07 15:19:05 +0530

    Update program1.js

--- program1.js
+++ program1.js
@@ -3,7 +3,22 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    
+    for(let i=0;i<s.length;i++){
+        let char==='('){
+            stack.push(')');
+        } else if (char === '[') {
+            stack.push(']');
+        } else if (char === '{') {
+            stack.push('}');
+        } 
+        // If it's a closing bracket, check if it matches the top of the stack
+        else if (char === ')' || char === ']' || char === '}') {
+            if (stack.pop() !== char) {
+                return false;  // Mismatch found
+            }
+        }
+    }
+     return stack.length === 0;
 };
 
 module.exports = { isValid };
