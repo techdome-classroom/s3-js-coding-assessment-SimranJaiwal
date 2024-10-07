669c0be968d21d8aae3bd709e17825583beecb2b
SimranJaiwal <161817125+SimranJaiwal@users.noreply.github.com>
2024-10-07 15:22:05 +0530

    Update program2.js

--- program2.js
+++ program2.js
@@ -3,7 +3,31 @@
  * @return {number}
  */
 var romanToInt = function(s) {
+    const romanMap = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
     
+    let total = 0;
+
+    for (let i = 0; i < s.length; i++) {
+        let current = romanMap[s[i]];
+        let next = romanMap[s[i + 1]];
+
+        if (current < next) {
+            total -= current;
+        } else {
+            // Otherwise, add it
+            total += current;
+        }
+    }
+
+    return total;
 };
 
 
